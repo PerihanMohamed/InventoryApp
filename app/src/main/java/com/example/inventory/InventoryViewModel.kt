4f0da9761a8abad157c4aa7baff986d4af386e7a
perihan_mohamed <perihan.mohamed92@gmail.com>
2021-12-19 14:46:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.inventory
+
+import androidx.lifecycle.*
+import com.example.inventory.data.Item
+import com.example.inventory.data.ItemDao
+import kotlinx.coroutines.launch
+
+/**
+ * View Model to keep a reference to the Inventory repository and an up-to-date list of all items.
+ *
+ */
+class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
+
+    /**
+     * Inserts the new Item into database.
+     */
+    fun addNewItem(itemName: String, itemPrice: String, itemCount: String) {
+        val newItem = getNewItemEntry(itemName, itemPrice, itemCount)
+        insertItem(newItem)
+    }
+
+    val allItems: LiveData<List<Item>> = itemDao.getItems().asLiveData()
+
+    /**
+     * Launching a new coroutine to insert an item in a non-blocking way
+     */
+    private fun insertItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.insert(item)
+        }
+    }
+     private fun updateItem(item : Item) {
+        viewModelScope.launch {
+            itemDao.update(item)
+        }
+    }
+
+     fun deleteItem(item: Item){
+        viewModelScope.launch {
+            itemDao.delete(item)
+        }
+    }
+
+    fun sellItem(item: Item) {
+
+        if (item.quantityInStock > 0){
+            val newItem = item.copy(quantityInStock = item.quantityInStock -1 )
+            updateItem(newItem)
+        }
+
+    }
+
+
+    /**
+     * Returns true if the EditTexts are not empty
+     */
+
+    fun isQuantityValid (item: Item) : Boolean {
+        return item.quantityInStock >0
+    }
+
+    fun isEntryValid(itemName: String, itemPrice: String, itemCount: String): Boolean {
+        if (itemName.isBlank() || itemPrice.isBlank() || itemCount.isBlank()) {
+            return false
+        }
+        return true
+    }
+
+    fun retrieveItem (id : Int) : LiveData <Item>{
+        return itemDao.getItem(id).asLiveData()
+    }
+
+    /**
+     * Returns an instance of the [Item] entity class with the item info entered by the user.
+     * This will be used to add a new entry to the Inventory database.
+     */
+    private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String): Item {
+        return Item(
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
+}
+
+/**
+ * Factory class to instantiate the [ViewModel] instance.
+ */
+class InventoryViewModelFactory(private val itemDao: ItemDao) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(InventoryViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return InventoryViewModel(itemDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
